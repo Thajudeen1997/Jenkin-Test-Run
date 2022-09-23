7104157e7418d8c80c533618703950675853e1bf
Faster <Faster@Coolama-Tatta>
2022-09-23 11:15:03 +0530

    commit 5

---
+++ src/test/java/demoRunTest/DemoTest2.java
@@ -0,0 +1,21 @@
+package demoRunTest;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	@Test
+	public void Test1() {
+		System.out.println("inside test2.1");
+	}
+	@Test
+	public void Test2() {
+		System.out.println("inside test2.2");
+	}	
+	@Test
+	public void Test3() {
+		System.out.println("inside test2.3");
+	}	
+	
+}
+
+
